7dbec0e94950766dd9de3d18ae9cd283095e84c0
pekape <37801685+pekape@users.noreply.github.com>
2018-03-27 21:21:44 +0300

    1.4

--- 1.1-1.5_kurssi/src/index.js
+++ 1.1-1.5_kurssi/src/index.js
@@ -11,36 +11,39 @@ const Osa = (props) => (
 
 const Sisalto = (props) => (
   <div>
-    <Osa osa={props.osa1} />
-    <Osa osa={props.osa2} />
-    <Osa osa={props.osa3} />
+    <Osa osa={props.osat[0]} />
+    <Osa osa={props.osat[1]} />
+    <Osa osa={props.osat[2]} />
   </div>
 )
 
 const Yhteensa = (props) => (
-  <p>Yhteensä {props.tehtavia} tehtävää</p>
+  <p>Yhteensä {props.osat[0].tehtavia + props.osat[1].tehtavia
+             + props.osat[2].tehtavia} tehtävää</p>
 )
 
 const App = () => {
   const kurssi = 'Half Stack -sovelluskehitys'
-  const osa1 = {
-    nimi: 'Reactin perusteet',
-    tehtavia: 10
-  }
-  const osa2 = {
-    nimi: 'Tiedonvälitys propseilla',
-    tehtavia: 7
-  }
-  const osa3 = {
-    nimi: 'Komponenttien tila',
-    tehtavia: 14
-  }
+  const osat = [
+    {
+      nimi: 'Reactin perusteet',
+      tehtavia: 10
+    },
+    {
+      nimi: 'Tiedonvälitys propseilla',
+      tehtavia: 7
+    },
+    {
+      nimi: 'Komponenttien tila',
+      tehtavia: 14
+    }
+  ]
 
   return (
     <div>
       <Otsikko kurssi={kurssi} />
-      <Sisalto osa1={osa1} osa2={osa2} osa3={osa3} />
-      <Yhteensa tehtavia={osa1.tehtavia + osa2.tehtavia + osa3.tehtavia} />
+      <Sisalto osat={osat} />
+      <Yhteensa osat={osat} />
     </div>
   )
 }
