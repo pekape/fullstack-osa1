ef73909f105e4602f606ec6cc3b4d80ed1543d06
pekape <37801685+pekape@users.noreply.github.com>
2018-03-27 21:29:56 +0300

    1.5

--- 1.1-1.5_kurssi/src/index.js
+++ 1.1-1.5_kurssi/src/index.js
@@ -17,33 +17,39 @@ const Sisalto = (props) => (
   </div>
 )
 
-const Yhteensa = (props) => (
-  <p>Yhteensä {props.osat[0].tehtavia + props.osat[1].tehtavia
-             + props.osat[2].tehtavia} tehtävää</p>
-)
+const Yhteensa = (props) => {
+  let summa = 0
+  props.osat.forEach(osa => summa += osa.tehtavia)
+
+  return (
+    <p>Yhteensä {summa} tehtävää</p>
+  )
+}
 
 const App = () => {
-  const kurssi = 'Half Stack -sovelluskehitys'
-  const osat = [
-    {
-      nimi: 'Reactin perusteet',
-      tehtavia: 10
-    },
-    {
-      nimi: 'Tiedonvälitys propseilla',
-      tehtavia: 7
-    },
-    {
-      nimi: 'Komponenttien tila',
-      tehtavia: 14
-    }
-  ]
+  const kurssi = {
+    nimi: 'Half Stack -sovelluskehitys',
+    osat: [
+      {
+        nimi: 'Reactin perusteet',
+        tehtavia: 10
+      },
+      {
+        nimi: 'Tiedonvälitys propseilla',
+        tehtavia: 7
+      },
+      {
+        nimi: 'Komponenttien tila',
+        tehtavia: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Otsikko kurssi={kurssi} />
-      <Sisalto osat={osat} />
-      <Yhteensa osat={osat} />
+      <Otsikko kurssi={kurssi.nimi} />
+      <Sisalto osat={kurssi.osat} />
+      <Yhteensa osat={kurssi.osat} />
     </div>
   )
 }
