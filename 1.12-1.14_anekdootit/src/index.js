eda282b5e1352f6b32b9e852c986d383b337383b
pekape <37801685+pekape@users.noreply.github.com>
2018-03-29 15:31:55 +0300

    1.14

--- 1.12-1.14_anekdootit/src/index.js
+++ 1.12-1.14_anekdootit/src/index.js
@@ -2,8 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 class App extends React.Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
     this.state = {
       selected: 0,
       votes: [0, 0, 0, 0, 0, 0]
